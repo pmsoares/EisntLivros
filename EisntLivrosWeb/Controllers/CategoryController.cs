9fbcf345c885a76956d43112f471c503ea934e27
Pedro Soares <pmsoares@gmail.com>
2022-12-12 23:23:53 +0000

    Alterações

---
+++ EisntLivrosWeb/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using EisntLivrosWeb.Data;
+using EisntLivrosWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EisntLivrosWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db) => _db = db;
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoriesList = _db.Categories;
+            return View(objCategoriesList);
+        }
+    }
+}
---
+++ EisntLivrosWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EisntLivrosWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EisntLivrosWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
